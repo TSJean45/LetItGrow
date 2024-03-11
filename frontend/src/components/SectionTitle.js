ad48040c099129d7efc46cdeca3d7b90fba986f7
JEAN\szuje <szujean45@gmail.com>
2024-03-12 00:38:42 +0800

    Section Title and Section Description

---
+++ frontend/src/components/SectionTitle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SectionTitle = ({ children }) => {
+  return (
+    <h2 className="text-6xl text-center font-title py-5 text-gray-800 mb-4">{children}</h2>
+  );
+};
+
+export default SectionTitle;
