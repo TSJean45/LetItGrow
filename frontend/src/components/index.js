e2a743c0a0522e7382ba9166d0f0329a42a35128
JEAN\szuje <szujean45@gmail.com>
2024-03-20 07:19:03 +0800

    Add sidebar for dashboard

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -10,3 +10,6 @@ export { default as Button } from './Button';
 export { default as ImageCarousel } from './ImageCarousel';
 export { default as Contact } from './Contact';
 export { default as Footer } from './Footer';
+export { default as DashboardNavbar } from './DashboardNavbar';
+export { default as DashboardSidebar } from './DashboardSidebar';
+export { default as NavbarSelect } from './NavbarSelect';
