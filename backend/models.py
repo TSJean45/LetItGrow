5f2295bbb6994ee3adb8534576010c9cf39c8ccb
JEAN\szuje <szujean45@gmail.com>
2024-04-11 10:17:24 +0800

    Setup MySQL database

---
+++ backend/models.py
@@ -0,0 +1,30 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+db = SQLAlchemy()
+
+class Users(db.Model):
+    __table_name__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(255), nullable=False)
+    lastName = db.Column(db.String(255), nullable=False)
+    userName = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+class Accounts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('users.id'), nullable=False)
+    account_type = db.Column(db.String(50), nullable=False)
+    
+    user = relationship('Users', backref='accounts', lazy=True)
+
+class Farms(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    account_id = db.Column(db.Integer, ForeignKey('accounts.id'), nullable=False)
+    farm_name = db.Column(db.String(255), nullable=False)
+    farm_location = db.Column(db.String(255), nullable=False)
+
+    account = relationship('Accounts', backref='farms', lazy=True)
