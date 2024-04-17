eb4cde84b8d3e3d7f83ee7f233a31fd314e22e14
JEAN\szuje <szujean45@gmail.com>
2024-04-18 05:07:21 +0800

    Fix plant simulation bug

--- frontend/src/pages/MyGarden.jsx
+++ frontend/src/pages/MyGarden.jsx
@@ -1,95 +1,95 @@
-import React, { useState, useEffect } from 'react';
-import { DashboardSidebar, DashboardNavbar } from '../components';
-import { RoughNotation } from 'react-rough-notation';
-import './MyGarden.scss';
-import { FaPlus } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
-import { plants } from '../constants/plants';
-import Select from 'react-select';
-import Box from '@mui/material/Box';
-import Slider from '@mui/material/Slider';
-import { Bars } from 'react-loading-icons';
+import React, { useState, useEffect } from "react";
+import { DashboardSidebar, DashboardNavbar } from "../components";
+import { RoughNotation } from "react-rough-notation";
+import "./MyGarden.scss";
+import { FaPlus } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { plants } from "../constants/plants";
+import Select from "react-select";
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
+import { Bars } from "react-loading-icons";
 
-import germination from '../assets/germination.png';
-import seedling from '../assets/seedling.png';
-import vegetative from '../assets/vegetative-growth.png';
-import buddev from '../assets/bud-development.png';
-import flowering from '../assets/flowering.png';
-import fruit from '../assets/fruit-formation.png';
-import ripening from '../assets/ripening.png';
-import tomato from '../assets/tomato.png';
+import germination from "../assets/germination.png";
+import seedling from "../assets/seedling.png";
+import vegetative from "../assets/vegetative-growth.png";
+import buddev from "../assets/bud-development.png";
+import flowering from "../assets/flowering.png";
+import fruit from "../assets/fruit-formation.png";
+import ripening from "../assets/ripening.png";
+import tomato from "../assets/tomato.png";
 
 const Groq = require("groq-sdk");
 const groq = new Groq({
   dangerouslyAllowBrowser: true,
-  apiKey: "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"
+  apiKey: "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",
 });
 
 const PlantButton = ({ label, imageSrc, onClick, selected }) => {
   return (
-    <button className={selected ? 'clicked' : ''} onClick={onClick}>
+    <button className={selected ? "clicked" : ""} onClick={onClick}>
       {label} <img src={imageSrc} alt="" />
     </button>
   );
 };
 
 const soil = [
-  { label: 'Loamy Soil', value: 'Loamy Soil' },
-  { label: 'Sandy Soil', value: 'Sandy Soil' },
-  { label: 'Clay Soil', value: 'Clay Soil' },
-  { label: 'Silt Soil', value: 'Silt Soil' },
-  { label: 'Peat Soil', value: 'Peat Soil' },
-  { label: 'Chalky Soil', value: 'Chalky Soil' },
-  { label: 'Rocky Soil', value: 'Rocky Soil' },
-  { label: 'Acidic Soil', value: 'Acidic Soil' },
-  { label: 'Saline Soil', value: 'Saline Soil' },
-  { label: 'Alkaline Soil', value: 'Alkaline Soil' },
+  { label: "Loamy Soil", value: "Loamy Soil" },
+  { label: "Sandy Soil", value: "Sandy Soil" },
+  { label: "Clay Soil", value: "Clay Soil" },
+  { label: "Silt Soil", value: "Silt Soil" },
+  { label: "Peat Soil", value: "Peat Soil" },
+  { label: "Chalky Soil", value: "Chalky Soil" },
+  { label: "Rocky Soil", value: "Rocky Soil" },
+  { label: "Acidic Soil", value: "Acidic Soil" },
+  { label: "Saline Soil", value: "Saline Soil" },
+  { label: "Alkaline Soil", value: "Alkaline Soil" },
 ];
 
 const fertilizer = [
-  { label: 'All-purpose fertilizer', value: 'All-purpose fertilizer' },
-  { label: 'Nitrogen-based fertilizer', value: 'Nitrogen-based fertilizer' },
+  { label: "All-purpose fertilizer", value: "All-purpose fertilizer" },
+  { label: "Nitrogen-based fertilizer", value: "Nitrogen-based fertilizer" },
   {
-    label: 'Phosphorus-based fertilizer',
-    value: 'Phosphorus-based fertilizer',
+    label: "Phosphorus-based fertilizer",
+    value: "Phosphorus-based fertilizer",
   },
-  { label: 'Potassium-based fertilizer', value: 'Potassium-based fertilizer' },
-  { label: 'Organic compost', value: 'Organic compost' },
-  { label: 'Fish emulsion fertilizer', value: 'Fish emulsion fertilizer' },
-  { label: 'Bone meal fertilizer', value: 'Bone meal fertilizer' },
-  { label: 'Seaweed fertilizer', value: 'Seaweed fertilizer' },
-  { label: 'Manure-based fertilizer', value: 'Manure-based fertilizer' },
-  { label: 'Liquid kelp fertilizer', value: 'Liquid kelp fertilizer' },
+  { label: "Potassium-based fertilizer", value: "Potassium-based fertilizer" },
+  { label: "Organic compost", value: "Organic compost" },
+  { label: "Fish emulsion fertilizer", value: "Fish emulsion fertilizer" },
+  { label: "Bone meal fertilizer", value: "Bone meal fertilizer" },
+  { label: "Seaweed fertilizer", value: "Seaweed fertilizer" },
+  { label: "Manure-based fertilizer", value: "Manure-based fertilizer" },
+  { label: "Liquid kelp fertilizer", value: "Liquid kelp fertilizer" },
 ];
 
 const light = [
-  { label: 'Sunlight', value: 'Sunlight' },
-  { label: 'LED grow lights', value: 'LED grow lights' },
-  { label: 'Fluorescent grow lights', value: 'Fluorescent grow lights' },
+  { label: "Sunlight", value: "Sunlight" },
+  { label: "LED grow lights", value: "LED grow lights" },
+  { label: "Fluorescent grow lights", value: "Fluorescent grow lights" },
   {
-    label: 'High-pressure sodium (HPS) grow lights',
-    value: 'High-pressure sodium (HPS) grow lights',
+    label: "High-pressure sodium (HPS) grow lights",
+    value: "High-pressure sodium (HPS) grow lights",
   },
   {
-    label: 'Metal halide (MH) grow lights',
-    value: 'Metal halide (MH) grow lights',
+    label: "Metal halide (MH) grow lights",
+    value: "Metal halide (MH) grow lights",
   },
   {
-    label: 'Compact fluorescent lamps (CFL)',
-    value: 'Compact fluorescent lamps (CFL)',
+    label: "Compact fluorescent lamps (CFL)",
+    value: "Compact fluorescent lamps (CFL)",
   },
-  { label: 'Incandescent grow lights', value: 'Incandescent grow lights' },
-  { label: 'Halogen lamps', value: 'Halogen lamps' },
+  { label: "Incandescent grow lights", value: "Incandescent grow lights" },
+  { label: "Halogen lamps", value: "Halogen lamps" },
   {
-    label: 'Ceramic metal halide (CMH) grow lights',
-    value: 'Ceramic metal halide (CMH) grow lights',
+    label: "Ceramic metal halide (CMH) grow lights",
+    value: "Ceramic metal halide (CMH) grow lights",
   },
-  { label: 'Induction grow lights', value: 'Induction grow lights' },
+  { label: "Induction grow lights", value: "Induction grow lights" },
 ];
 
 const MyGarden = () => {
   const [currentStep, setCurrentStep] = useState(0);
-  const [selection, setSelection] = useState('');
+  const [selection, setSelection] = useState("");
   const navigate = useNavigate();
   const [isClicked, setIsClicked] = useState(false);
   const [resultData, setResultData] = useState(null);
@@ -97,6 +97,7 @@ const MyGarden = () => {
 
   // get the selected value/adjustments of the plants
   const [selectedPlant, setSelectedPlant] = useState(null);
+  const [current, setCurrent] = useState(null);
   const [selectedStage, setSelectedStage] = useState(null);
   const [temperature, setTemperature] = useState(50);
   const [watering, setWatering] = useState(50);
@@ -107,15 +108,19 @@ const MyGarden = () => {
   const [page2Complete, setPage2Complete] = useState(false);
 
   // species of plant
-  const handlePlantChange = (selectedOption) => {
-    console.log("Selected Plant:", selectedOption);
-    setSelectedPlant(selectedOption);
+  const handlePlantChange = (selected) => {
+    console.log("Selected Plant:", selected);
+
+    setSelectedPlant(selected);
+    setCurrent(selected);
     setPage1Complete(true);
   };
+  
 
   // growing stage
   const handleStageChange = (selectedStage) => {
     setSelectedStage(selectedStage);
+    console.log(selectedStage);
     setPage1Complete(true);
   };
 
@@ -152,6 +157,26 @@ const MyGarden = () => {
     setPage2Complete(true);
   };
 
+  const handleNext = (selected) => {
+    if (selected === "plus") {
+      setSelection(selected);
+      setCurrentStep((prevStep) => Math.min(prevStep + 1, 2)); // Max step 2
+    } else {
+      setSelectedPlant(selected);
+      setSelection(""); // Reset the selection state after setting the plant
+      setCurrentStep((prevStep) => Math.min(prevStep + 1, 2)); // Max step 2
+    }
+  };
+
+  const passPlant = (selected) => {
+    setSelectedPlant(selected);
+    setCurrentStep((prevStep) => Math.min(prevStep + 1, 2)); // Max step 2
+  };
+
+  const handleCancel = () => {
+    navigate("/");
+  };
+
   const result = async () => {
     const data = {
       plant: selectedPlant,
@@ -163,43 +188,51 @@ const MyGarden = () => {
       light: selectedLight,
     };
 
+    console.log("Dataaaa received: ", data);
+
     // Construct the message content using the data object
     let messageContent = `Take note that temperature is in Celsius and water measurement is in millilitres. The use of Fahrenheit is. Act as a horticultural expert and given the condition of a ${data.plant} plant/tree where its growth stage is ${data.stage} under growing conditions where temperature is ${data.temperature} Celsius, ${data.watering} ml watering daily, planted in ${data.soil}, using ${data.fertilizer} and under ${data.light}. Describe the good and bad effects that the growing condition of the ${data.plant} plant/tree bringing and provide guidance for mitigating the bad effects. Keep this simple and sweet as you are communicating with a person who is new to farming.`;
 
     setLoading(true);
     try {
       // Call Groq function to get plant simulation result
-      const simulationResult = await getGroqPlantSimulation(messageContent, data);
+      const simulationResult = await getGroqPlantSimulation(
+        messageContent,
+        data
+      );
       // Check if simulationResult is not undefined or null
       if (simulationResult) {
         setResultData(simulationResult);
       } else {
-        console.error('Invalid data received from Groq:', simulationResult);
+        console.error("Invalid data received from Groq:", simulationResult);
       }
       setLoading(false);
     } catch (error) {
-      console.error('Error retrieving data from Groq:', error);
+      console.error("Error retrieving data from Groq:", error);
       setLoading(false);
     }
   };
 
   const getGroqPlantSimulation = async (messageContent, data) => {
     try {
-      console.log('Received data:', data);
+      console.log("Received data:", data);
       // Replace the slots in messageContent with actual data values
-      messageContent = messageContent.replace(/\${(.*?)}/g, (match, property) => {
-        return data[property.trim()] || match;
-      });
+      messageContent = messageContent.replace(
+        /\${(.*?)}/g,
+        (match, property) => {
+          return data[property.trim()] || match;
+        }
+      );
 
       // Call Groq SDK to get plant simulation
       const response = await groq.chat.completions.create({
         messages: [
           {
             role: "user",
-            content: messageContent // Pass the modified messageContent
-          }
+            content: messageContent, // Pass the modified messageContent
+          },
         ],
-        model: "mixtral-8x7b-32768"
+        model: "mixtral-8x7b-32768",
       });
 
       // Extract the content of the message
@@ -207,19 +240,9 @@ const MyGarden = () => {
 
       return simulationResult;
     } catch (error) {
-      console.error('Error retrieving data from Groq:', error);
+      console.error("Error retrieving data from Groq:", error);
       throw error; // Rethrow the error to handle it where the function is called
     }
-  };
-
-  // change to next step
-  const handleNext = (selected) => {
-    setSelection(selected);
-    setCurrentStep((prevStep) => Math.min(prevStep + 1, 2)); // Max step 2
-  };
-
-  const handleCancel = () => {
-    navigate('/');
   };
 
   return (
@@ -232,7 +255,7 @@ const MyGarden = () => {
       {/* select or add new plant in my garden */}
       {currentStep === 0 && (
         <div className="add-plant">
-          <div style={{ textAlign: 'center', marginBottom: '10px' }}>
+          <div style={{ textAlign: "center", marginBottom: "10px" }}>
             <RoughNotation
               type="highlight"
               show={true}
@@ -242,8 +265,8 @@ const MyGarden = () => {
               padding="0"
               strokeWidth="0"
               style={{
-                fontSize: '50px',
-                fontFamily: 'baloo',
+                fontSize: "50px",
+                fontFamily: "baloo",
               }}
             >
               My Garden
@@ -251,12 +274,12 @@ const MyGarden = () => {
           </div>
 
           <plants>
-            <button onClick={() => handleNext('plus')}>
+            <button onClick={() => passPlant("tomato")}>
               Tomato
               <img src={tomato} alt="" />
             </button>
 
-            <button onClick={() => handleNext('plus')}>
+            <button onClick={() => handleNext("plus")}>
               <FaPlus size={40} />
             </button>
           </plants>
@@ -266,10 +289,16 @@ const MyGarden = () => {
       {/* add new plant */}
       {currentStep === 1 && (
         <div className="plant-info">
-          <searchbar>
-            <Select options={plants} onChange={handlePlantChange} required />
-          </searchbar>
-
+          {selection === "plus" && (
+            <searchbar>
+              <Select
+                options={plants}
+                onChange={(selectedOption) => handlePlantChange(selectedOption.value)}
+                value={current}
+                required
+              />
+            </searchbar>
+          )}
           <input type="name" required placeholder="Give a Name to Your Plant" />
           <div className="stages">
             <h4>Current Stages of Plants/Tree Growth</h4>
@@ -278,51 +307,51 @@ const MyGarden = () => {
               <PlantButton
                 label="Germination"
                 imageSrc={germination}
-                onClick={() => handleStageChange('Germination')}
-                selected={selectedStage === 'Germination'}
+                onClick={() => handleStageChange("Germination")}
+                selected={selectedStage === "Germination"}
               />
               <PlantButton
                 label="Seedling"
                 imageSrc={seedling}
-                onClick={() => handleStageChange('Seedling')}
-                selected={selectedStage === 'Seedling'}
+                onClick={() => handleStageChange("Seedling")}
+                selected={selectedStage === "Seedling"}
               />
               <PlantButton
                 label="Vegetative Growth"
                 imageSrc={vegetative}
-                onClick={() => handleStageChange('Vegetative Growth')}
-                selected={selectedStage === 'Vegetative Growth'}
+                onClick={() => handleStageChange("Vegetative Growth")}
+                selected={selectedStage === "Vegetative Growth"}
               />
               <PlantButton
                 label="Bud Development"
                 imageSrc={buddev}
-                onClick={() => handleStageChange('Bud Development')}
-                selected={selectedStage === 'Bud Development'}
+                onClick={() => handleStageChange("Bud Development")}
+                selected={selectedStage === "Bud Development"}
               />
               <PlantButton
                 label="Flowering"
                 imageSrc={flowering}
-                onClick={() => handleStageChange('Flowering')}
-                selected={selectedStage === 'Flowering'}
+                onClick={() => handleStageChange("Flowering")}
+                selected={selectedStage === "Flowering"}
               />
               <PlantButton
                 label="Fruit Formation"
                 imageSrc={fruit}
-                onClick={() => handleStageChange('Fruit Formation')}
-                selected={selectedStage === 'Fruit Formation'}
+                onClick={() => handleStageChange("Fruit Formation")}
+                selected={selectedStage === "Fruit Formation"}
               />
               <PlantButton
                 label="Ripening"
                 imageSrc={ripening}
-                onClick={() => handleStageChange('Ripening')}
-                selected={selectedStage === 'Ripening'}
+                onClick={() => handleStageChange("Ripening")}
+                selected={selectedStage === "Ripening"}
               />
             </selection>
           </div>
 
           <button
             className="proceed"
-            onClick={() => handleNext('plus')}
+            onClick={() => handleNext("plus")}
             disabled={!page1Complete}
           >
             proceed
@@ -333,7 +362,7 @@ const MyGarden = () => {
       {currentStep === 2 && (
         <div className="simulation">
           <results>
-            <div style={{ textAlign: 'center', marginBottom: '10px' }}>
+            <div style={{ textAlign: "center", marginBottom: "10px" }}>
               <RoughNotation
                 type="highlight"
                 show={true}
@@ -343,9 +372,9 @@ const MyGarden = () => {
                 padding="0"
                 strokeWidth="0"
                 style={{
-                  fontSize: '50px',
-                  fontFamily: 'open sans',
-                  fontWeight: '700',
+                  fontSize: "50px",
+                  fontFamily: "open sans",
+                  fontWeight: "700",
                 }}
               >
                 Results
@@ -419,7 +448,11 @@ const MyGarden = () => {
               </searchbar>
             </light>
 
-            <button className="confirm" onClick={result} disabled={!page2Complete}>
+            <button
+              className="confirm"
+              onClick={result}
+              disabled={!page2Complete}
+            >
               Confirm
             </button>
           </adjustment>
