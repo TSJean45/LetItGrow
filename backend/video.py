e3f439c87ae766224f7c0b52d31bd6c1a77fad4c
Chiauwen <chiauwen28@gmail.com>
2024-04-18 22:04:31 +0800

    solve disease detect bug and upload new video

--- backend/video.py
+++ backend/video.py
@@ -1,39 +1,44 @@
 import cv2
 from ultralytics import YOLO
 
-def play_video(video_path):
-    video = cv2.VideoCapture(video_path)
-    model = YOLO("backend/AI models/disease-detection-yolov8n.pt")
+# def play_video(video_path):
+#     video = cv2.VideoCapture(video_path)
+#     model = YOLO("backend/AI models/disease-detection-yolov8n.pt")
 
-    while True:
-        ret, frame = video.read()
+#     while True:
+#         ret, frame = video.read()
 
-        if not ret:
-            break
+#         if not ret:
+#             break
         
-        results = model(frame)
+#         model.track(frame, persist=True, show=True, conf=0.3)
         
-        for r in results:
-            boxes = r.boxes
+#         # for r in results:
+#         #     boxes = r.boxes
 
-            for box in boxes:
-                conf = box.conf
-                cls = int(box.cls[0])
+#         #     for box in boxes:
+#         #         conf = box.conf
+#         #         cls = int(box.cls[0])
                 
-                if conf > 0.2:
+#         #         if conf > 0.2:
 
-                    x1, y1, x2, y2 = box.xyxy[0]
-                    x1, y1, w, h = int(x1), int(y1), int(x2 - x1), int(y2 - y1)
+#         #             x1, y1, x2, y2 = box.xyxy[0]
+#         #             x1, y1, w, h = int(x1), int(y1), int(x2 - x1), int(y2 - y1)
 
-                    cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
+#         #             cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
 
-        cv2.imshow("Video", frame)
+#         cv2.imshow("Video", frame)
 
-        if cv2.waitKey(25) & 0xFF == ord("q"):
-            break
+#         if cv2.waitKey(25) & 0xFF == ord("q"):
+#             break
 
-    video.release()
-    cv2.destroyAllWindows()
+#     video.release()
+#     cv2.destroyAllWindows()
 
-video_path = "backend/sources/grape2.mp4"
-play_video(video_path)
+# video_path = "backend/sources/grape2.mp4"
+# play_video(video_path)
+
+
+model = YOLO("backend/AI models/disease-detection-yolov8n.pt")
+model.track("backend/sources/leaf2.mp4", persist=True, show=True, conf=0.2, tracker="bytetrack.yaml")
+        
