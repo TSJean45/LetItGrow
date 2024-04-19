e663ad01cc37d2574f5be807f115b9122d24fc76
JEAN\szuje <szujean45@gmail.com>
2024-04-19 13:09:31 +0800

    fix bug

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
 import joblib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from crud import soil_monitoring_bp,personal_plant_bp, space_mapping_bp
 from ultralytics import YOLO
 import cv2
 import os
 import shutil
+import json
+import random
+import logging
 
 # from flask_sqlalchemy import SQLAlchemy
 # from dotenv import load_dotenv
@@ -46,10 +48,6 @@ model = YOLO("../backend/AI models/disease-detection-yolov8n.pt")
 #         fields = ("id", "firstName", "lastName", "userName", "email", "password")
 
 # users_schema = UserSchema(many=True)
-
-app.register_blueprint(soil_monitoring_bp)
-app.register_blueprint(space_mapping_bp)
-app.register_blueprint(personal_plant_bp)
 
 crop_models = {
     "banana": "../backend/AI models/Banana_random_forest_model.pkl",
@@ -124,6 +122,187 @@ def plant_simulation():
 
     return jsonify({"result": str(result)})
 
+@app.route('/personal-plant/<int:data_id>', methods=['GET'])
+def get_plant_data(data_id):
+    try:
+        with open("data/personal.json", 'r') as plant_file:
+            plant_data = json.load(plant_file)
+            for item in plant_data:
+                if item['id'] == data_id:
+                    return jsonify(item)
+            abort(404, description="Data not found for the provided ID")
+    except FileNotFoundError:
+        abort(500, description="Plant data file not found")
+    except Exception as e:
+        abort(500, description=str(e))
+
+@app.route('/personal-plant/all', methods=['GET'])
+def get_all_plant():
+    try:
+        with open("data/personal.json", 'r') as plant_file:
+            plant_data = json.load(plant_file)
+            return jsonify(plant_data)
+    except FileNotFoundError:
+        abort(500, description="Map data file not found")
+    except Exception as e:
+        abort(500, description=str(e))
+
+    # In case of any other unexpected error
+    abort(500, description="Failed to fetch space mapping data")
+
+@app.route('/personal-plant/<int:data_id>', methods=['PUT'])
+def update_room_temperature(data_id):
+    logging.basicConfig(level=logging.DEBUG) 
+    
+    with open('data/plantTemp.json', 'r') as file:
+        temp_data = json.load(file)
+        logging.debug(f'iot_temp_data: {temp_data}')
+    
+    random_index = random.randint(0, len(temp_data) - 1)
+    temp_object = temp_data[random_index]
+    logging.debug(f'temp_object: {temp_object}')
+
+    with open('data/personal.json', 'r') as map_file:
+        map_data = json.load(map_file)
+        logging.debug(f'map_data before update: {map_data}')
+
+    for item in map_data:
+        if item['id'] == data_id:
+            item['temperature'] = temp_object['temp']
+            break
+    logging.debug(f'map_data after update: {map_data}')
+
+    # Write updated map_data back to map.json
+    with open('data/personal.json', 'w') as map_file:
+        json.dump(map_data, map_file, indent=4)
+    
+    return jsonify({'message': 'Data updated successfully', 'updated_data': item})
+
+@app.route('/soil-monitoring/<int:data_id>', methods=['GET'])
+def get_soil_monitoring_data(data_id):
+    try:
+        with open("data/map.json", 'r') as map_file:
+            map_data = json.load(map_file)
+            for item in map_data:
+                if item['id'] == data_id:
+                    return jsonify(item)
+            abort(404, description="Data not found for the provided ID")
+    except FileNotFoundError:
+        abort(500, description="Map data file not found")
+    except Exception as e:
+        abort(500, description=str(e))
+
+    # In case of any other unexpected error
+    abort(500, description="Failed to fetch soil monitoring data")
+
+@app.route('/soil-monitoring/<int:data_id>', methods=['PUT'])
+def update_soil_monitoring_data(data_id):
+    logging.basicConfig(level=logging.DEBUG) 
+    
+    with open('data/iotTemp.json', 'r') as file:
+        iot_temp_data = json.load(file)
+        logging.debug(f'iot_temp_data: {iot_temp_data}')
+    
+    random_index = random.randint(0, len(iot_temp_data) - 1)
+    temp_object = iot_temp_data[random_index]
+    logging.debug(f'temp_object: {temp_object}')
+
+    with open('data/map.json', 'r') as map_file:
+        map_data = json.load(map_file)
+        logging.debug(f'map_data before update: {map_data}')
+
+    for item in map_data:
+        if item['id'] == data_id:
+            item['temp'] = temp_object['temp']
+            item['moisture'] = temp_object['moisture']
+            item['ph'] = temp_object['ph']
+            item['fertility'] = temp_object['fertility']
+            item['light'] = temp_object['light']
+            break
+    logging.debug(f'map_data after update: {map_data}')
+
+    # Write updated map_data back to map.json
+    with open('data/map.json', 'w') as map_file:
+        json.dump(map_data, map_file, indent=4)
+    
+    return jsonify({'message': 'Data updated successfully', 'updated_data': item})
+
+@app.route('/space-mapping', methods=['POST'])
+def add_space_mapping_data():
+    try:
+        data = request.get_json()
+        print(data)
+
+        required_fields = ['name', 'plantVariety', 'plantingDate', 'plantCounts', 'note', 'growingArea', 'polygons']
+
+        for field in required_fields:
+            if field not in data:
+                abort(400, description=f"Field '{field}' is required")
+                
+        
+        data.setdefault('temp', None)       # Default to None if not provided
+        data.setdefault('moisture', None)   # Default to None if not provided
+        data.setdefault('ph', None)         # Default to None if not provided
+        data.setdefault('fertility', "")    # Default to an empty string if not provided
+        data.setdefault('light', "")   
+        data.setdefault('percentage', "50")  
+        data.setdefault('image', "mapImage-1.png")  
+        data.setdefault('pMessage', "Nearly all of the field has been watered")  
+        data.setdefault('status', "You haven't watered this field!")  # Default to an empty string if not provided
+        data.setdefault('icon', "bell")     # Default to "bell" if not provided
+        data.setdefault('button', True)     # Default to True if not provided
+
+
+
+        print(data)
+        # Write the data to map.json
+        with open('data/map.json', 'r+') as map_file:
+            map_data = json.load(map_file)
+            new_id = max(map_data, key=lambda x: x['id'])['id'] + 1
+            print(new_id)
+            data['id'] = new_id
+            map_data.append(data)
+            map_file.seek(0)
+            json.dump(map_data, map_file, indent=4)
+            map_file.truncate()
+
+        return jsonify({'message': 'Data added successfully', 'new_data': data}), 201
+    except KeyError:
+        abort(500, description="Invalid data format: 'id' field not found in map_data")
+    except Exception as e:
+        abort(500, description=str(e))
+
+
+@app.route('/space-mapping/<int:data_id>', methods=['GET'])
+def get_space(data_id):
+    try:
+        with open("data/map.json", 'r') as map_file:
+            map_data = json.load(map_file)
+            for item in map_data:
+                if item['id'] == data_id:
+                    return jsonify(item)
+            abort(404, description="Data not found for the provided ID")
+    except FileNotFoundError:
+        abort(500, description="Map data file not found")
+    except Exception as e:
+        abort(500, description=str(e))
+
+    # In case of any other unexpected error
+    abort(500, description="Failed to fetch soil monitoring data")
+
+@app.route('/space-mapping/all', methods=['GET'])
+def get_all_space_mapping():
+    try:
+        with open("data/map.json", 'r') as map_file:
+            map_data = json.load(map_file)
+            return jsonify(map_data)
+    except FileNotFoundError:
+        abort(500, description="Map data file not found")
+    except Exception as e:
+        abort(500, description=str(e))
+
+    # In case of any other unexpected error
+    abort(500, description="Failed to fetch space mapping data")
 
 @app.route("/disease_detect_image", methods=["POST"])
 def disease_detect_image():
